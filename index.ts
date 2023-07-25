c37018a3d89e21e8cf8c1744715dc03cdfb7287d
Muhammad462 <139973126+Muhammad462@users.noreply.github.com>
2023-07-25 15:31:17 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,4 @@
+// task 8: store your favorite number in a variable ,then using that variable ,create a message that reveals your favorate number,print that message
+var favoritenumber = "333"
+var message = `My favorite number is${favoritenumber}`
+console.log(message)
